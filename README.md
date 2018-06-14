a2c6d080f724967b4461403100c45c510c68238e
Pierre Marchand <pierremarc07@gmail.com>
2018-06-14 17:13:11 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# solar-sim
+
+
+Commencer par un ```npm install``` pour recuperer typescript, puis commencer a bosser dans src/index.ts, puis lancer ```npm run build``` pour compiler sous lib/index.js.
---
+++ src/index.ts
@@ -0,0 +1,16 @@
+/*
+ * src/index.ts
+ *
+ * 
+ * Copyright (C) 2015-2017 Pierre Marchand <pierremarc07@gmail.com>
+ * Copyright (C) 2017 Pacôme Béru <pacome.beru@gmail.com>
+ *
+ *  License in LICENSE file at the root of the repository.
+ */
+
+
+export type Name = string;
+
+
+export const helloWorld =
+    (n: Name) => `Hello ${n}!`;
