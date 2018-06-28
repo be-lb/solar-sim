132455097d9a848ecc5de321680e748f82c99be3
nobohan <juminet@gmail.com>
2018-06-28 13:03:21 +0200

    default parameters for main function - proof-of-concept

--- src/index.ts
+++ src/index.ts
@@ -14,33 +14,34 @@ import {solarFinSim} from './run';
 // TODO: These objects Building and Roofs should be constructed by parsing some geojson from lot-1
 
 
-// interface roof {
-//     'area': number;
-//     'productivity': number;
-// }
+interface roof {
+    'area': number;
+    'productivity': number;
+}
 
 interface inputs {
     'nYears': number;
     'currentYear': number;
     'typology': string;
-    //'roofs':  roof;
+    'roofs':  roof[];
 }
 
 
 const inputsFactory = (
-        nYears=25, /* default */
-        currentYear=2018, /* default */
-        typology='residential' /* default */
-      //  roofs: roof[] /* required */
+        roofs: roof[], /* required */
+        nYears = 25, /* default */
+        currentYear = 2018, /* default */
+        typology = 'residential' /* default */
     ): inputs => {
     /**
-    * Build the inputs for solarFinSim with default values
+    * @param roofs - Array of roof objects
+    * Build the inputs for solarFinSim with default and required values
     */
     return {
         nYears: nYears,
         currentYear: currentYear,
         typology: typology,
-        //roofs: roofs
+        roofs: roofs
     }
 };
 
@@ -49,15 +50,15 @@ interface outputs {
 };
 
 // Set parameters
-let nYears: number = 20;
-//let currentYear: number = 2018; // TODO: get from (new Date()).getFullYear()?
+//let nYears: number = 20;
+let currentYear: number = 2016; // TODO: get from (new Date()).getFullYear()?
 //let typology: string = 'residential';
-// let roofs: roof[] = [
-//     {'area': 60, 'productivity': 1200},
-// ];
+let roofs: roof[] = [
+    {'area': 60, 'productivity': 1200},
+];
+console.log(roofs);
 
-
-let inputs_test = inputsFactory(nYears);
+let inputs_test = inputsFactory(roofs, undefined, currentYear);
 console.log(inputs_test);
 
 let results = solarFinSim(inputs_test);
