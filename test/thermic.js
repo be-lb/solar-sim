805d2e5b0048b7faebde2ba2dfe40f29043fddba
nobohan <juminet@gmail.com>
2018-08-27 12:29:44 +0200

    add tests for thermic

--- test/thermic.js
+++ test/thermic.js
@@ -11,7 +11,6 @@ describe('Thermic', function() {
   describe('Thermic()', function() {
     it('should expose an object', function () {
       var t = new thermic.Thermic(5, 30, 'electric', -9999, -9999, 3, 2500, 0.06);
-      console.log(t)
       expect(t).to.be.a('object');
     });
     it('should expose a function', function () {
@@ -45,7 +44,7 @@ describe('Thermic', function() {
   describe('computeBalances()', function() {
     it('should return true', function() {
       var b = new building.Building(0.2);
-      var r = new roof.Roof(30, 950, 30, 180, 'poly', b);
+      var r = new roof.Roof(30, 950, 30, 110, 'poly', b);
       b.roofs = [r];
       var t = new thermic.Thermic(5, 30, 'electric', -9999, -9999, 3, 2500, 0.06);
       t.computeCost();
@@ -57,7 +56,91 @@ describe('Thermic', function() {
       var nYears = 25;
 
       var expected = [-4358, -4760, -5262, -5664, -6066, -6563, -6965, -7367, -7858, -8260, -8662, -9148, -9550, -9952, -10433, -10835, -11237, -11714, -12116, -12518, -12991, -13393, -13795, -14263, -14665];
-      expect(thermic.computeBalances(t,f,nYears).VANminusConsoWithSolar.map(x => Math.round(x/1000)*1000)).to.eql(expected.map(x => Math.round(x/1000)*1000));
+      expect(thermic.computeBalances(t,f,nYears).VANminusConsoWithSolar.map(x => Math.round(x/100)*100)).to.eql(expected.map(x => Math.round(x/100)*100));
+    });
+    it('should return true', function() {
+      var b = new building.Building(0.2);
+      var r = new roof.Roof(30, 950, 30, 110, 'poly', b);
+      b.roofs = [r];
+      var t = new thermic.Thermic(5, 30, 'electric', -9999, -9999, 3, 2500, 0.06);
+      t.computeCost();
+      t.computeAnnualMaintenanceCost();
+      t.building = b;
+      t.computeSolarProduction();
+      t.computePumpConsumption();
+      var f = new financial.Financial(-9999, -9999, 0.06, -9999, false, 3, 0.018);
+      var nYears = 25;
+
+      var expected = [-715, -1429, -2144, -2859, -3573, -4288, -5003, -5717, -6432, -7147, -7861, -8576, -9291, -10005, -10720, -11435, -12149, -12864, -13579, -14293, -15008, -15723, -16437, -17152, -17867];
+      expect(thermic.computeBalances(t,f,nYears).VANminusConsoWithoutSolar.map(x => Math.round(x/100)*100)).to.eql(expected.map(x => Math.round(x/100)*100));
+    });
+  });
+  describe('computeThermicGain = 3201', function() {
+    it('should return true', function() {
+      var b = new building.Building(0.2);
+      var r = new roof.Roof(30, 950, 30, 110, 'poly', b);
+      b.roofs = [r];
+      var t = new thermic.Thermic(5, 30, 'electric', -9999, -9999, 3, 2500, 0.06);
+      t.computeCost();
+      t.computeAnnualMaintenanceCost();
+      t.building = b;
+      t.computeSolarProduction();
+      t.computePumpConsumption();
+      var f = new financial.Financial(-9999, -9999, 0.06, -9999, false, 3, 0.018);
+      var nYears = 25;
+
+      var expected = 3201;
+      expect(Math.round(thermic.computeThermicGain(t,f,nYears))).to.be.equal(expected);
+    });
+  });
+  describe('computeActualReturnTimeThermic = 13.8', function() {
+    it('should return true', function() {
+      var b = new building.Building(0.2);
+      var r = new roof.Roof(30, 950, 30, 110, 'poly', b);
+      b.roofs = [r];
+      var t = new thermic.Thermic(5, 30, 'electric', -9999, -9999, 3, 2500, 0.06);
+      t.computeCost();
+      t.computeAnnualMaintenanceCost();
+      t.building = b;
+      t.computeSolarProduction();
+      t.computePumpConsumption();
+      var f = new financial.Financial(-9999, -9999, 0.06, -9999, false, 3, 0.018);
+      var nYears = 25;
+
+      var expected = 13.8;
+      expect(Math.round(thermic.computeActualReturnTimeThermic(t,f,nYears)*10)/10).to.be.equal(expected);
+    });
+  });
+  describe('computeProductionPrices()', function() {
+    it('should return true', function() {
+      var b = new building.Building(0.2);
+      var r = new roof.Roof(30, 950, 30, 110, 'poly', b);
+      b.roofs = [r];
+      var t = new thermic.Thermic(5, 30, 'electric', -9999, -9999, 3, 2500, 0.06);
+      t.computeCost();
+      t.computeAnnualMaintenanceCost();
+      t.building = b;
+      t.computeSolarProduction();
+      t.computePumpConsumption();
+      var nYears = 25;
+
+      var expected = 0.13;
+      expect(Math.round(thermic.computeProductionPrices(t,nYears).productionPriceWithSubsidies*100)/100).to.be.equal(expected);
+    });
+    it('should return true', function() {
+      var b = new building.Building(0.2);
+      var r = new roof.Roof(30, 950, 30, 110, 'poly', b);
+      b.roofs = [r];
+      var t = new thermic.Thermic(5, 30, 'electric', -9999, -9999, 3, 2500, 0.06);
+      t.computeCost();
+      t.computeAnnualMaintenanceCost();
+      t.building = b;
+      t.computeSolarProduction();
+      t.computePumpConsumption();
+      var nYears = 25;
+
+      var expected = 0.21;
+      expect(Math.round(thermic.computeProductionPrices(t,nYears).productionPriceWithoutSubsidies*100)/100).to.be.equal(expected);
     });
   });
 });
